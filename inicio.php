c4388237085998f2cd615b0894c7f2ca53b7fb3d
manuelafasantos0406-cmd <manuelafasantos0406@gmail.com>
2026-06-30 16:39:20 -0300

    first commit

---
+++ inicio.php
@@ -0,0 +1,24 @@
+<?php
+//inclui arquivo responsavel pela conexao
+include "conexao.php";
+
+
+$numero1 = 10;
+$numero2 = 13;
+$resultado = $numero1 + $numero2;
+
+
+//linguagem sql
+
+$sql =" INSERT INTO calculo(numero1, numero2, resultado)
+VALUES($numero1, $numero2,  $resultado)";
+
+
+if ($conexao->query($sql)) {
+    echo "<br> Dados salvo com sucesso!"; 
+}else{
+    echo "<br> Erro ao salvar";
+}
+
+?>
+
